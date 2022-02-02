cc738014b1451056845616a2fd189512edc71042
Sebastian Franco <diego.sebas.franco@gmail.com>
2022-02-02 17:34:19 -0600

    Tests

--- CalculatorTesting.java
+++ CalculatorTesting.java
@@ -1,16 +1,25 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.*;
+
 
 
 public class CalculatorTesting {
     CalculatorG3 calc = new CalculatorG3();
+    StackG3 st = new StackG3();
 
     @Test
-    public void testCambiarLista(){
-        CalculatorTesting test = new CalculatorTesting();
-        Assert.assertEquals(0, 0);
-        
-    
+    void testCalculate() throws Exception {
+        assertEquals(Math.abs(Math.log(0)), this.calc.calculate());
+
     }
+    
+    @Test
+    void testEmpty() throws Exception {
+        assertEquals(expected, actual);
+    }
+
+    
+    
 }
